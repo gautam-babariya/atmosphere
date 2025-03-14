b5b5265e518a4aae8b24f10035315c94c3812832
gautam <gautambabariya0125@gmail.com>
2025-03-14 23:32:47 +0530

    dashdone

--- Frontend/src/Pages/Maindashboard/Maindashboard.jsx
+++ Frontend/src/Pages/Maindashboard/Maindashboard.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './Maindashboard.css'
 import Dashboard2 from '../../Components/Dashboard2/Dashboard2'
 import Dashboard from '../../Components/Dashboard/Dashboard'
+import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
 
 function Maindashboard() {
   return (
-    <div>
+    <div id='maindash-div'>
+      <Navbar />
       <Dashboard />
       <Dashboard2 />
+      <Footer />
     </div>
   )
 }
